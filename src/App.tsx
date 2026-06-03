8ae911909b5605727f3d75e8978534d7f5bcd9ff
Lenka H <hubackova.lenka@gmail.com>
2026-06-03 15:53:58 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+      <section id="center">
+        <div className="hero">
+        </div>
+        <div>
+          <h1>Fens test :)</h1>
+        </div>
+      </section>
+
+  )
+}
+
+export default App
